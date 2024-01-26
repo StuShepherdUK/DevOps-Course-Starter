33642ed729bbfdb42fa3c09f190a9035b9d18c3f
sds1bal <stuart.shepherd@uk.bosch.com>
2024-01-26 11:31:19 +0000

    module3 - part 2 - step 2 - client fixture

--- todo_app/tests/test_view_model.py
+++ todo_app/tests/test_view_model.py
@@ -1,7 +1,22 @@
 import pytest
+from dotenv import load_dotenv, find_dotenv
+
 from todo_app.classes.view_model import ViewModel
 from todo_app.classes.item_class import Item
+from todo_app import app
 
+@pytest.fixture
+def client():
+    # Use our test integration config instead of the 'real' version
+    file_path = find_dotenv('.env.test')
+    load_dotenv(file_path, override=True)
+
+    # Create the new app.
+    test_app = app.create_app()
+
+    # Use the app to create a test_client that can be used in our tests.
+    with test_app.test_client() as client:
+        yield client
 
 @pytest.fixture
 def test_data():
@@ -13,7 +28,7 @@ def test_data():
     items.append(Item('id5','item_name_5','todo'))
     return items
 
-def test_view_model_done_property_returns_done_items_only(test_data):
+def test_view_model_done_property_returns_done_items_only(client,test_data):
     # arrange
     items = test_data
     view_model = ViewModel(items)
@@ -22,7 +37,7 @@ def test_view_model_done_property_returns_done_items_only(test_data):
     # assert
     assert len(filtered_items) == 2
 
-def test_view_model_todo_property_returns_todo_items_only(test_data):
+def test_view_model_todo_property_returns_todo_items_only(client,test_data):
     # arrange
     items = test_data
     view_model = ViewModel(items)
