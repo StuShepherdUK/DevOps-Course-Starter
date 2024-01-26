7a5638c828c1cfb54c8eb0831fe2c5d6f3caa557
sds1bal <stuart.shepherd@uk.bosch.com>
2024-01-26 09:52:45 +0000

    Updated after Module 2 Review

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,5 +1,5 @@
 from flask import session
-from ..classes.item_class import Item
+from todo_app.classes.item_class import Item
 import json
 import os
 import requests
@@ -10,18 +10,7 @@ import uuid
     Therefore verify=False is necessary on requests call as a work-around
 """
 
-trello_boards_url       = "https://api.trello.com/1/boards/"
-trello_cards_url        = "https://api.trello.com/1/cards"
-trello_board_id         = os.environ.get('TRELLO_BOARD_ID','')
-trello_board_list_id_todo = os.environ.get('TRELLO_BOARD_LIST_ID_TODO','')
-trello_board_list_id_done = os.environ.get('TRELLO_BOARD_LIST_ID_DONE','')
-trello_api_key          = os.environ.get('TRELLO_API_KEY','')
-trello_api_token        = os.environ.get('TRELLO_API_TOKEN','')
-trello_api_security     = "key="+trello_api_key+"&token="+trello_api_token
-trello_default_get_header   = {"Accept": "*/*"}
-trello_default_put_header   = {"Accept": "application/json"}
-trello_default_post_header   = {"Accept": "application/json"}
-cards_status_options    = ['all','closed','none','open','visible']
+
 
 def get_items():
     """
@@ -30,6 +19,15 @@ def get_items():
     Returns:
         list: The list of saved items.
     """
+    trello_boards_url       = "https://api.trello.com/1/boards/"
+    trello_board_id         = os.environ.get('TRELLO_BOARD_ID','')
+    trello_api_key          = os.environ.get('TRELLO_API_KEY','')
+    trello_api_token        = os.environ.get('TRELLO_API_TOKEN','')
+    trello_api_security     = "key="+trello_api_key+"&token="+trello_api_token
+    trello_default_get_header   = {"Accept": "*/*"}
+    trello_board_list_id_todo = os.environ.get('TRELLO_BOARD_LIST_ID_TODO','')
+    trello_board_list_id_done = os.environ.get('TRELLO_BOARD_LIST_ID_DONE','')
+    
     get_item_req_url = trello_boards_url+trello_board_id+'/cards/?'+trello_api_security 
     response = requests.request("GET", get_item_req_url, data='',  headers=trello_default_get_header, verify=False)
 
@@ -54,22 +52,40 @@ def get_items():
 
     return_items = {'todo':todo_items,'done':done_items}
     session.clear()
-    return session.get('items', return_items.copy())
+    #return session.get('items', return_items.copy())
+    return return_items
 
 
 def add_item(title):
+
+    trello_cards_url        = "https://api.trello.com/1/cards"
+    trello_board_list_id_todo = os.environ.get('TRELLO_BOARD_LIST_ID_TODO','')
+    trello_api_key          = os.environ.get('TRELLO_API_KEY','')
+    trello_api_token        = os.environ.get('TRELLO_API_TOKEN','')
+    trello_default_post_header   = {"Accept": "application/json"}
+
+
     query = {
         'idList':   trello_board_list_id_todo,
         'key':      trello_api_key,
         'token':    trello_api_token,
         'name':     title
     }
-    response = requests.request("POST",trello_cards_url,headers=trello_default_post_header,params=query,verify=False)
+    requests.request("POST",trello_cards_url,headers=trello_default_post_header,params=query,verify=False)
     get_items()
     return query
 
 
 def update_item(item_to_update_id,source):
+
+
+    trello_cards_url        = "https://api.trello.com/1/cards"
+    trello_board_list_id_todo = os.environ.get('TRELLO_BOARD_LIST_ID_TODO','')
+    trello_board_list_id_done = os.environ.get('TRELLO_BOARD_LIST_ID_DONE','')
+    trello_api_key          = os.environ.get('TRELLO_API_KEY','')
+    trello_api_token        = os.environ.get('TRELLO_API_TOKEN','')
+    trello_default_put_header   = {"Accept": "application/json"}
+
     if source.lower() == "todo":
         target_list = trello_board_list_id_done
     elif source.lower() == "done":
